dccb8a759d25da30408de62bcca340c455310664
Davina T W <91788095+dolaemonngeong@users.noreply.github.com>
2023-04-11 08:19:23 +0700

    Initial Commit

---
+++ 0706012110020-Davina T W-AFL3/_706012110020_Davina_T_W_AFL3App.swift
@@ -0,0 +1,17 @@
+//
+//  _706012110020_Davina_T_W_AFL3App.swift
+//  0706012110020-Davina T W-AFL3
+//
+//  Created by MacBook Pro on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct _706012110020_Davina_T_W_AFL3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
